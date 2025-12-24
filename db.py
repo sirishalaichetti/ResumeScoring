2b12f6ef82d2bffd94909151ed05339572049938
Sireesha <sirishalaichetti03@gmail.com>
2025-12-24 11:10:33 +0530

    changed db

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 import psycopg2
 
 conn = psycopg2.connect(
-    host="localhost",
-    database="resume_parser",
-    user="postgres",
-    password="root",
+    host="dpg-d55nko95pdvs73c8kfr0-a.virginia-postgres.render.com",
+    database="resumedata_g1e2",
+    user="siri",
+    password="nBcSaQaChFrFVOY3srpsoDzfE99AatT9",
     port=5432
 )
+
